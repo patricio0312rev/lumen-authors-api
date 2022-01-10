0b2835192f6002b0bcb56077cb515ceb114c72eb
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-10 11:02:54 -0500

    Configurando dabatase seeder y factories en Model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call('AuthorsTableSeeder');
+        Author::factory()->count(50)->create();
     }
 }
